0830226b5efa9e87d9ffe8970e6dc7f30f7425c7
Anye Happiness Ade <innovacttechnologies@gmail.com>
2025-05-06 13:29:47 +0100

    Fixed some issues with Typescript

--- src/vs/workbench/contrib/void/browser/react/src/util/services.tsx
+++ src/vs/workbench/contrib/void/browser/react/src/util/services.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 /*--------------------------------------------------------------------------------------
  *  Copyright 2025 Glass Devtools, Inc. All rights reserved.
  *  Licensed under the Apache License, Version 2.0. See LICENSE.txt for more information.
